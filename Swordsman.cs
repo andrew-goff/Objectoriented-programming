051df2d7fd32e4fcb735b84804077eda3cc8d795
andrew-goff <andrew.goff@hotmail.co.uk>
2014-12-23 20:45:41 +0000

    Swordman Class
    Added in the Swordman Class which inherits from Friendly Fighter class
    and contains Weapon . SWORD

---
+++ Swordsman.cs
@@ -0,0 +1,24 @@
+﻿//AndrewG8460
+//Program file that creates the Swordsman class and inherits from FriendlyFighter
+using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EnemyFighters;
+
+namespace ConsoleDrivenBattleGame
+{
+    public class Swordsman : FriendlyFighter
+    {
+        public Swordsman(int id): base(id, Weapon.SWORD, Magic.NONE)
+        {
+
+        }
+
+        public string ToString()
+        {
+            return "Swordsman " + base.ToString();
+        }
+    }
+}
