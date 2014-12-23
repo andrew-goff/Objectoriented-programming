92aaebfdad7a196f393ec02a30e3d099e4990eee
andrew-goff <andrew.goff@hotmail.co.uk>
2014-12-23 20:40:24 +0000

    Dragon Class
    Added in the Dragon Class which inherits from Friendly Fighter class and
    contains Weapon . FIRE

---
+++ Dragon.cs
@@ -0,0 +1,24 @@
+﻿//AndrewG8460
+//Program file that creates the dragon class and inherits from FriendlyFighter
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EnemyFighters;
+
+namespace ConsoleDrivenBattleGame
+{
+    public class Dragon : FriendlyFighter
+    {
+        public Dragon (int id) : base(id, Weapon.FIRE, Magic.NONE)
+        {  
+            
+            
+        }
+        
+        public string ToString()
+        {
+            return " Dragon" + base.ToString();
+        }
+    }
+}
